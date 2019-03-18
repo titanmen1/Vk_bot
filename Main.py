21a12d0ecaa54920502793ce4ab87537de0998c6
titanmen1 <zaq12123@mail.ru>
2019-03-18 17:48:53 +0300

    Версия 0.2

--- Main.py
+++ Main.py
@@ -2,6 +2,8 @@ import requests
 import vk_api
 from vk_api.upload import VkUpload
 from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+from vk_api.utils import get_random_id
 import random
 import json
 from vk_bot import pars
@@ -13,24 +15,14 @@ token = 'bbfd8e6e47808fbaedf251fd3d726ffa5cc127499b3612561ea8e4f8c57fd2120e17f0f
 
 
 vk_session = vk_api.VkApi(token=token)
-
-
 longpoll = VkLongPoll(vk_session)
-
 vk = vk_session.get_api()
-
-
 upload = vk_api.VkUpload(vk_session)
 
-# file = 'image/' + str(random.randint(1,19)) + '.jpg'
-#
-# photo1 = upload.photo_messages(file)
-#
-# vk_photo_url = photo1[0]['sizes'][-1]['url']
-#
-#
-# print(photo1, '\nLink: ', vk_photo_url)
-
+keyboard = VkKeyboard(one_time=True)
+keyboard.add_button('Цитата', color=VkKeyboardColor.DEFAULT)
+keyboard.add_line()
+keyboard.add_button('help', color=VkKeyboardColor.DEFAULT)
 
 
 
@@ -48,38 +40,31 @@ def messages_user():
                     file = 'image/' + str(random.randint(1, 30)) + '.jpg'
                     photo1 = upload.photo_messages(file)
 
-#                    print(event.text)
- #                   print(event.user_id)
                     vk.messages.send(  # Отправляем сообщение
                         user_id=event.user_id,
                         message=pars()[random.randint(1, 146)],
-                        random_id=random.randint(1, 10000000),
-                        attachment='photo{}_{}'.format(photo1[0]['owner_id'], photo1[0]['id'])
+                        random_id=get_random_id(),
+                        attachment='photo{}_{}'.format(photo1[0]['owner_id'], photo1[0]['id']),
+                        keyboard=keyboard.get_keyboard(),
                     )
+                    # vk.messages.send(  # Отправляем сообщение
+                    #     user_id=event.user_id,
+                    #     message='1',
+                    #     random_id=get_random_id(),
+                    #     keyboard=keyboard.get_keyboard(),
+                    # )
+
+
                     return messages_user()
             if event.text == 'help':
                 if event.from_user:
                     vk.messages.send(
                         user_id=event.user_id,
                         message="Слушай браток, я буду тебя развлекать, пока ты чалишься на нарах. Вводи команду 'Цитата' и я пришлю четкую цитату про нашу не легкую жизнь",
-                        random_id=random.randint(1, 10000000),
+                        random_id=get_random_id(),
+                        keyboard=keyboard.get_keyboard(),
                     )
 
-
-
-# def msg():
-#     vk.messages.send(  # Отправляем сообщение
-#         user_id=32483967,
-#         message=pars()[1],
-#         random_id=random.randint(1, 1000)
-#     )
-# schedule.every().day.at("21:30").do(msg)
-# while True:
-#     schedule.run_pending()
-#     time.sleep(1)
-
-   #             pfile = post(api.photos.getMessagesUploadServer(peer_id = update['object']['user_id'])['upload_url'], files = {'photo': open('python.jpeg', 'rb')}).json()
-  #              photo = api.photos.saveMessagesPhoto(server = pfile['server'], photo = pfile['photo'], hash = pfile['hash'])[0]
 
 
 
